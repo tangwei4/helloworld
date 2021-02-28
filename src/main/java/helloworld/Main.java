2dd08bbd8a7554d6b31ffeeeb8935c35c9e09ce9
唐威4 <2424176683@qq.com>
2021-02-28 17:01:00 +0800

    提交代码

---
+++ src/main/java/helloworld/Main.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ * @author 唐威
+ * @version V1.0
+ * @Package helloworld
+ * @date 2021/2/28 16:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("3.java");
+    }
+}
